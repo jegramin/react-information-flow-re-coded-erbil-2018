f37a2b30182c777e06f4c03c47d3cfacf428ffbc
Mohammed Ameen <jegroamin@gmail.com>
2018-09-14 19:16:28 +0000

    automatically backed up by learn

--- src/Tier3.js
+++ src/Tier3.js
@@ -1,7 +1,10 @@
+
 import React from 'react'
 
+
 const Tier3 = (props) => (
-  <div className="tier3" onClick={props.handleClick} style={{backgroundColor: props.color, color: props.color}}></div>
+  // this component does not need to change (but feel free to change however you like!)
+  <div className="tier3" onClick={props.handleChildClick} style={{backgroundColor: props.color, color: props.color}}></div>
 )
 
 export default Tier3
